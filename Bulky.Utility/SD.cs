dde1d422fe9d81a8bbc8b60c983ae485cafd1064
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2024-09-06 14:31:48 -0600

    Git blame  statics classes

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -8,24 +8,24 @@ namespace BulkyBook.Utility
 {
     public static class SD
     {
-        public const string Role_Customer = "Cliente";
-        public const string Role_Company = "Empresa";
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
         public const string Role_Admin = "Admin";
-        public const string Role_Employee = "Empleado";
+        public const string Role_Employee = "Employee";
 
-		public const string StatusPending = "Pendiente";
-		public const string StatusApproved = "Aprovado";
-		public const string StatusInProcess = "Procesando";
-		public const string StatusShipped = "Enviado;
-		public const string StatusCancelled = "Cancelado";
-		public const string StatusRefunded = "Reembolsado";
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "Processing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
 
-		public const string PaymentStatusPending = "Pendiente";
-		public const string PaymentStatusApproved = "Aprobado";
-		public const string PaymentStatusDelayedPayment = "Aprovado pago tardio.";
-		public const string PaymentStatusRejected = "Rechazado";
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
 
-        public const string SessionCart = "SesionCarritoDeCompras";
+        public const string SessionCart = "SessionShoppingCart";
 
     }
 
